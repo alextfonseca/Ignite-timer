51cbb2784fdbfd2128bf203f427692d13b06de26
Alex Teixeira da Fonseca <alexatf2014@gmail.com>
2022-08-30 10:45:36 -0300

    first commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,123 @@
+import { Play } from "phosphor-react";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
+
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountDownButton,
+  TaskInput,
+} from "./style";
+import { useState } from "react";
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, "Informe a tarefa"),
+  minutesAmount: zod
+    .number()
+    .min(5, "O ciclo precisa ser de no mínimo 5 minutos")
+    .max(60, "O ciclo precisa ser de no máximo 60 minutos"),
+});
+
+// cria a tipagem a partir das validações que colocamos
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
+
+interface Cycle {
+  id: string;
+  task: string;
+  minutesAmount: number;
+}
+
+export const Home = () => {
+  const [cycles, setCycles] = useState<Cycle[]>([]);
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+
+  const { register, handleSubmit, watch, formState, reset } =
+    useForm<NewCycleFormData>({
+      resolver: zodResolver(newCycleFormValidationSchema),
+      defaultValues: {
+        task: "",
+        minutesAmount: 0,
+      },
+    });
+
+  // apenas executa a função se os campos estiverem corretos
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    const id = String(new Date().getTime());
+
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+    };
+
+    // busca todos os estados antigos e adiciona um novo para servir como histórico
+    setCycles((state) => [...state, newCycle]);
+    setActiveCycleId(id);
+
+    reset(); // reseta os valores
+  }
+
+  const activeCycle = cycles.find((cycles) => cycles.id === activeCycleId);
+
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
+  // se tiver algum campo com erro ele retorna um objeto com a mensagem de erro
+  if (formState.errors) {
+    console.log(formState.errors);
+  }
+
+  const task = watch("task");
+  const isSubmitDisabled = !task;
+
+  return (
+    <HomeContainer>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            id="task"
+            type="text"
+            placeholder="Dê um nome para o seu projeto"
+            list="task-suggestions"
+            {...register("task")}
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+          </datalist>
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            id="minutesAmount"
+            type="number"
+            placeholder="00"
+            step={5}
+            {...register("minutesAmount", { valueAsNumber: true })}
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountDownButton type="submit" disabled={isSubmitDisabled}>
+          <Play size={24} />
+          Começar
+        </StartCountDownButton>
+      </form>
+    </HomeContainer>
+  );
+};
